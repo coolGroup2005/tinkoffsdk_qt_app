3a207d2187a4ae7c920e9b36171fb621d7b24cd9
ByloTonix <bylotonix@gmail.com>
2024-05-19 03:49:45 +0300

    modified akcii

--- apps/akcii.cpp
+++ apps/akcii.cpp
@@ -1,4 +1,5 @@
 #include "akcii.h"
+#include "qcustomplot.h"
 #include "ui_akcii.h"
 
 akcii::akcii(QWidget *parent)
@@ -6,9 +7,21 @@ akcii::akcii(QWidget *parent)
     , ui(new Ui::akcii)
 {
     ui->setupUi(this);
-}
+    this->resize(840, 520);
+    this->setFixedSize(this->size()); //lock an ability to change the size
 
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &akcii::updateDateTime);
+    timer->start(1000);
+}
 akcii::~akcii()
 {
     delete ui;
+}
+
+void akcii::updateDateTime()
+{
+    QDateTime currentDateTime = QDateTime::currentDateTime();
+    ui->dateLabel->setText(currentDateTime.toString("dddd, MMMM d"));
+    ui->timeLabel->setText(currentDateTime.toString("h:mm:ss"));
 }
