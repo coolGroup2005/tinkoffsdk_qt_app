4303dc6409a6d186e34088cf236a8cb349c5b279
pelancha <sobachekrokky@gmail.com>
2024-05-21 13:39:18 +0300

    update gitignore

--- apps/main.cpp
+++ apps/main.cpp
@@ -1,13 +1,15 @@
 // main.cpp
 #include <QApplication>
 #include "mainwindow.h"
-#include "invest/example.h"
+#include <iostream>
+// #include "invest/example.h"
 
 
 int main(int argc, char *argv[])
 {
-    MyInvest mi;
-    mi.getCandles();
+    std::cout << "OKKKKKK!";
+    // MyInvest mi;
+    // mi.getCandles();
     // QApplication app(argc, argv);
 
     // MainWindow mainWindow;
