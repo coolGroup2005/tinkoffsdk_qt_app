5a003ce65306e211ec96ebff6d83856e40c7e2fa
pelancha <sobachekrokky@gmail.com>
2024-05-21 15:41:17 +0300

    delete qcustomplot

--- apps/main.cpp
+++ apps/main.cpp
@@ -6,12 +6,12 @@
 
 int main(int argc, char *argv[])
 {
-    MyInvest mi;
-    mi.getCandles();
-    // QApplication app(argc, argv);
+    // MyInvest mi;
+    // mi.getCandles();
+    QApplication app(argc, argv);
 
-    // MainWindow mainWindow;
-    // mainWindow.show();
+    MainWindow mainWindow;
+    mainWindow.show();
 
-    // return app.exec();
+    return app.exec();
 }
--- src/invest/example.cpp
+++ src/invest/example.cpp
@@ -3,6 +3,8 @@
 #include "sandboxservice.h"
 #include "marketdataservice.h"
 #include "instrumentsservice.h"
+
+
 void MyInvest::getCandles() const
 {
     std::cout << "HEllo\n";
