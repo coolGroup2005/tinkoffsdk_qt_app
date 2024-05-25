db5a0404828474873da4ecfa5f9ef11964114d7f
pelancha <sobachekrokky@gmail.com>
2024-05-25 20:32:04 +0300

    add handling of seg fault

--- apps/main.cpp
+++ apps/main.cpp
@@ -2,12 +2,28 @@
 #include "mainwindow.h"
 // #include "invest/example.h"
 
+#include <signal.h>
+#include <QMessageBox>
+
+
+void manageSegFailure(int);
+
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    signal(SIGSEGV, manageSegFailure);
 
     MainWindow mainWindow;
     mainWindow.show();
 
     return app.exec();
+}
+
+
+void manageSegFailure(int signalCode)
+{
+    QMessageBox::critical(nullptr, "Error", "Access denied\nCheck access level of token in settings", QMessageBox::Ok);
+    signal(signalCode, SIG_DFL);
+    QApplication::exit(3);
 }
