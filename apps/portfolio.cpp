3266d062ca1cb67d39c2df7e13241ed6049138df
Pavel Sardak <glonomess@protonmail.com>
2024-05-26 14:26:13 +0300

    worcking positions show

--- apps/portfolio.cpp
+++ apps/portfolio.cpp
@@ -24,6 +24,7 @@
 #include "portfolio.h"
 #include <QVBoxLayout>
 #include <cstdlib>
+#include <QDebug> // for finding segmentation fault
 
 Portfolio::Portfolio(QWidget *parent) : QWidget(parent)
 {
@@ -40,11 +41,22 @@ Portfolio::Portfolio(QWidget *parent) : QWidget(parent)
     setLayout(layout);
 
     token = getenv("TOKEN");
-    client = new InvestApiClient("invest-public-api.tinkoff.ru:443", token.toStdString()); // sandbox-
+    client = new InvestApiClient("sandbox-invest-public-api.tinkoff.ru:443", token.toStdString()); // sandbox-
 
     auto accountService = std::dynamic_pointer_cast<Users>(client->service("users"));
+
+    if (!accountService) {
+        qDebug() << "Failed to get Users service";
+        return;
+    }
+
     auto accountListOfID = accountService->GetAccounts();
     auto accountID1 = dynamic_cast<GetAccountsResponse*>(accountListOfID.ptr().get());
+
+    if (!accountID1) {
+        qDebug() << "Failed to get accounts list";
+        return;
+    }
 
     for (int i = 0; i < accountID1->accounts_size(); ++i) {
         auto account = accountID1->accounts(i);
@@ -63,30 +75,79 @@ Portfolio::Portfolio(QWidget *parent) : QWidget(parent)
 
 void Portfolio::updateBalance(const QString& accountId)
 {
+
+    if (!client) {
+        qDebug() << "InvestApiClient is not initialized";
+        return;
+    }
+
     auto operationService = std::dynamic_pointer_cast<Operations>(client->service("operations"));
+
+    if (!operationService) {
+        qDebug() << "Failed to get Operations service";
+        return;
+    }
+
     auto instrumentsService = std::dynamic_pointer_cast<Instruments>(client->service("instruments"));
+
+    if (!instrumentsService) {
+        qDebug() << "Failed to get Instruments service";
+        return;
+    }
 
     auto portfolioRequest = operationService->GetPortfolio(accountId.toStdString(), PortfolioRequest_CurrencyRequest::PortfolioRequest_CurrencyRequest_RUB);
     auto portfolioAns = dynamic_cast<PortfolioResponse*>(portfolioRequest.ptr().get());
+
+    if (!portfolioAns) {
+        qDebug() << "Failed to get portfolio response";
+        return;
+    }
+
     auto portfolioValue = portfolioAns->total_amount_portfolio().units();
 
     balanceLabel->setText(QString::number(portfolioValue) + " " + QString::fromStdString(portfolioAns->total_amount_portfolio().currency()));
 
-    portfolioModel->removeRows(0, portfolioModel->rowCount());
+    portfolioModel->setRowCount(portfolioAns->positions_size());
+
+    // std::cout << "Я тут!_1" << '\n';
 
     for (int i = 0; i < portfolioAns->positions_size(); ++i) {
         const auto& position = portfolioAns->positions(i);
 
+        std::cout << portfolioRequest.ptr()->DebugString() << '\n';
+        
         auto instrumentUID = position.instrument_uid();
-        auto instrumentRequest = instrumentsService->ShareBy(tinkoff::public_::invest::api::contract::v1::InstrumentIdType::INSTRUMENT_ID_TYPE_UID, "", instrumentUID);
-        auto shareResponse = dynamic_cast<ShareResponse*>(instrumentRequest.ptr().get());
-        auto shareInfo = shareResponse->instrument();
-        auto shareName = shareInfo.name();
-        auto shareTicker = shareInfo.ticker();
+
+        // if (position.instrument_type() == "currency"){
+        //     auto instrumentRequest = instrumentsService->GetInstrumentBy(tinkoff::public_::invest::api::contract::v1::InstrumentIdType::INSTRUMENT_ID_TYPE_UID, "", instrumentUID);
+        //     auto instrumentResponse = dynamic_cast<CurrencyResponse*>(instrumentRequest.ptr().get());
+        // }
+
+        auto instrumentRequest = instrumentsService->GetInstrumentBy(tinkoff::public_::invest::api::contract::v1::InstrumentIdType::INSTRUMENT_ID_TYPE_UID, "", instrumentUID);
+        auto instrumentResponse = dynamic_cast<InstrumentResponse*>(instrumentRequest.ptr().get());
+
+        // auto instrumentRequest = instrumentsService->ShareBy(tinkoff::public_::invest::api::contract::v1::InstrumentIdType::INSTRUMENT_ID_TYPE_UID, "", instrumentUID);
+        // auto shareResponse = dynamic_cast<ShareResponse*>(instrumentRequest.ptr().get());
+
+        if (!instrumentResponse) {
+            qDebug() << "Failed to get instrumentResponse";
+            return;
+        }
+
+        std::cout << "Я тут!" << '\n';
+
+
+        auto instrumentInfo = instrumentResponse->instrument();
+        auto instrumentName = instrumentInfo.name();
+        auto instrumentTicker = instrumentInfo.ticker();
+
+        // auto shareInfo = shareResponse->instrument();
+        // auto shareName = shareInfo.name();
+        // auto shareTicker = shareInfo.ticker();
 
         QList<QStandardItem*> rowItems;
-        rowItems.append(new QStandardItem(QString::fromStdString(shareTicker)));
-        rowItems.append(new QStandardItem(QString::fromStdString(shareName)));
+        rowItems.append(new QStandardItem(QString::fromStdString(instrumentTicker)));
+        rowItems.append(new QStandardItem(QString::fromStdString(instrumentName)));
         rowItems.append(new QStandardItem(QString::number(position.quantity().units())));
         rowItems.append(new QStandardItem(QString::number(position.current_price().units()) + " " + QString::fromStdString(position.current_price().currency())));
         portfolioModel->appendRow(rowItems);
